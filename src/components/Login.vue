d49a5c16b0be9a955a48d3838eb23c1bdaf47a7e
stevenchendan <qwe16165850@gmail.com>
2019-09-22 00:42:48 +1000

    Add login component

---
+++ src/components/Login.vue
@@ -0,0 +1,28 @@
+<template>
+  
+<div id="login">
+  <div>Login</div>
+</div>
+
+</template>
+
+<script>
+export default {
+  name: 'Login',
+  data () {
+    return {
+
+    }
+  },
+  methods: {
+
+  }
+  
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+
+</style>
